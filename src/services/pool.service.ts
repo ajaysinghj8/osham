73b2783633e5dce3cd79ac05acaec42b509af8b7
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-17 23:52:02 +0530

    add logs pool service

--- src/services/pool.service.ts
+++ src/services/pool.service.ts
@@ -1,5 +1,7 @@
 import { RedisSub, RedisPub } from "../storage/redis.store";
 import { Cache } from "./cache.service";
+import * as Debug from 'debug';
+const logger = Debug('api-cache-proxy:service:pool');
 
 export class RequestPool {
     static pool: Set<string> = new Set();
@@ -13,10 +15,12 @@ export class RequestPool {
     }
 
     static wait(key: string) {
+        logger(`wating for ${key}`);
         return new Promise((resolve) => {
             function handler(channel: string, data: any) {
                 if (channel !== key) return;
                 resolve(JSON.parse(data));
+                logger(`respond for ${key}`);
                 RedisSub.off(key, handler);
             }
             RedisSub.on('message', handler);
@@ -24,6 +28,7 @@ export class RequestPool {
     }
 
     static async putAndPublish(key: string, data: any, expires: number) {
+        logger(`putting and publishing for ${key}`);
         RedisSub.subscribe(key);
         await Cache.put(key, data, expires);
         RedisPub.publish(key, JSON.stringify(data));
