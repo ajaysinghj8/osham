8268fc1047646dc674296ca751f2d88bdf1f2dfb
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-17 10:46:54 +0530

    Layout down Structure

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { App } from './server';
+import { getCacheConfig } from './config.reader';
+import { RouteTimeReqRes } from './middlewares/responseTime';
+import { createNameSpaceHandler } from './middlewares/nameSpaceHandler';
+
+const config = getCacheConfig();
+
+/**
+ * For each namespace
+ *  create handler
+ */
+
+for (const key in config) {
+    switch (key) {
+        case 'version': break;
+        case 'x-reponse':
+            App.use(RouteTimeReqRes);
+            break;
+        default:
+            // it is namespace
+            App.use(createNameSpaceHandler(key, config[key]));
+    }
+}
+console.log(config);
