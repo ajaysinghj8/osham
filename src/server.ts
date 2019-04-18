14e3c8253c2f6eb5fb5f76cbbe545f758543c033
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-18 12:51:49 +0530

    env PORT to number

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ const logger = Debug('acp:server');
 
 export const App = new Koa();
 
-App.listen(process.env.PORT || 26192, () => {
+App.listen(+process.env.PORT || 26192, () => {
     logger(`listing on ${process.env.PORT}`);
 });
