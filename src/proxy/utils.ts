06e767fabfcbf2bb289b8e25c37921fa5feec339
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-22 01:44:53 +0530

    pass ctx in write header

--- src/proxy/utils.ts
+++ src/proxy/utils.ts
@@ -1,9 +1,13 @@
 import { ServerResponse, OutgoingHttpHeaders } from "http";
+import { Context } from "../ctx.provider";
 
-export function writeHeaders(headers: OutgoingHttpHeaders, response?: ServerResponse) {
+export function writeHeaders(headers: OutgoingHttpHeaders, ctx?: Context) {
     const headersMap: any = {};
-    const setHeader = response ? (field: string, value: any) => response.setHeader(field, value)
-        : (field: string, value: any) => headersMap[field] = value;
+    const setCtxHeaders = (field: string, value: any) => {
+        ctx.set(field, value);
+        headersMap[field] = value;
+    };
+    const setHeader = ctx ? setCtxHeaders : (field: string, value: any) => headersMap[field] = value;
     for (const field in headers) {
         if (field === undefined) continue;
         const value = headers[field];
