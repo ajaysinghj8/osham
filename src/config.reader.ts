77b7f58cee78fb71e06cf02a8e08bb3da76cf781
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-24 20:46:29 +0530

    support env var in config yml resolve #30

--- src/config.reader.ts
+++ src/config.reader.ts
@@ -2,10 +2,24 @@ import { safeLoad } from 'js-yaml';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
+function supplant(o: any = {}) {
+    return this.replace(/\${([^{}]*)}/g,
+        function (a: string, b: string) {
+            const r: any = o[b];
+            if (r === undefined) {
+                throw new Error(`Please set environment value of ${b}`);
+            }
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+}
+
 export function getCacheConfig() {
     try {
         const configFilePath = join(process.cwd(), 'cache-config.yml');
-        const config = safeLoad(readFileSync(configFilePath, 'utf-8'));
+        const config = safeLoad(
+            supplant.call(readFileSync(configFilePath, 'utf-8'), process.env)
+        );
         //@todo : validate yml config rules
         return config;
     }
