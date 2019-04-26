2b910b666f393d299ad2764287732a2bc25b24d6
ajaysinghj8 <ajay.singh@srijan.net>
2019-04-26 23:49:19 +0530

    ensures request pool clear request after timeout resolve #36

--- src/services/pool.service.ts
+++ src/services/pool.service.ts
@@ -1,8 +1,11 @@
 import { Cache } from './cache.service';
 import * as Debug from 'debug';
 import { EventEmitter } from 'events';
+import { timeOutResponse } from '../utils';
 
 const logger = Debug('acp:service:pool');
+
+const TIMEOUT: number = (+process.env.TIMEOUT) || 5000;
 
 export class RequestPool {
     static pool: Set<string> = new Set();
@@ -19,26 +22,35 @@ export class RequestPool {
     static wait(key: string) {
         logger(`wating for ${key}`);
         return new Promise((resolve) => {
+
+            const timeout = setTimeout(() => {
+                RequestPool.pool.delete(key);
+                RequestPool.ee.off(key, handler);
+                logger(`TIMEOUT ${key}`);
+                handler(timeOutResponse('RequestPool failed'));
+            }, TIMEOUT);
+
             function handler(data: any) {
+                clearTimeout(timeout);
                 resolve(data);
                 logger(`respond for ${key}`);
             }
+
             RequestPool.ee.once(key, handler);
         });
     }
 
     static async putAndPublish(key: string, data: any, expires: number) {
         logger(`putting and publishing for ${key}`);
+        RequestPool.pool.delete(key);
         RequestPool.ee.emit(key, data);
         await Cache.put(key, data, expires);
-        RequestPool.pool.delete(key);
         return data;
     }
 
     static async errorAndPublish(key: string, data: any) {
         logger(`error and publishing for ${key}`);
-        RequestPool.ee.emit(key, data);
         RequestPool.pool.delete(key);
+        RequestPool.ee.emit(key, data);
     }
-
 }
